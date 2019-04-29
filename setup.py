2e95b3b0c06e72bbc898da84def70e16239a0c83
jpmieville <jpmieville@gmail.com>
2019-04-29 15:44:33 +0200

    Added a license file and added pyodbc as requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='iseries',
-    version='1.0',
+    version='1.1',
     author="Jean-Paul Mieville",
     author_email="jpmieville@gmail.com",
     description="An library to help to make an ODBC connection with an iSeries or i5 IBM server",
@@ -13,6 +13,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jpmieville/iseries.git",
     packages=setuptools.find_packages(),
+    install_requires=["pyodbc",
+                      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
